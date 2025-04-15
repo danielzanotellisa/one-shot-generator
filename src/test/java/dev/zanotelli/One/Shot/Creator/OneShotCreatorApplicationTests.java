00719808277d78d5e4f2e452e729381137d396fc
Daniel Zanotelli <danielzanotelli12@gmail.com>
2025-04-14 23:51:30 -0300

    first commit

---
+++ src/test/java/dev/zanotelli/One/Shot/Creator/OneShotCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zanotelli.One.Shot.Creator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneShotCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
