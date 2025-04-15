c1e01dafed9fa23e47df1d47eb84a3b9ff433f7d
Daniel Zanotelli <danielzanotelli12@gmail.com>
2025-04-15 08:22:19 -0300

    feat: Cria os ending points da API para GET, POST e DELETE de personagens

--- src/main/java/dev/zanotelli/One/Shot/Creator/controller/PersonagemController.java
+++ src/main/java/dev/zanotelli/One/Shot/Creator/controller/PersonagemController.java
@@ -1,8 +1,12 @@
 package dev.zanotelli.One.Shot.Creator.controller;
 
+import dev.zanotelli.One.Shot.Creator.model.Personagem;
 import dev.zanotelli.One.Shot.Creator.service.PersonagemService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/personagem")
@@ -13,6 +17,26 @@ public class PersonagemController {
     public PersonagemController(PersonagemService service) {
         this.service = service;
     }
+
+    @GetMapping()
+    public ResponseEntity<List<Personagem>> getAll() {
+        return ResponseEntity.ok(service.getAll);
+    }
+
+    //POST
+
+    @PostMapping()
+    public ResponseEntity<Personagem> save(@RequestBody Personagem request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(request));
+    }
+
+    //DELETE
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
 
 
 }
--- src/main/java/dev/zanotelli/One/Shot/Creator/service/PersonagemService.java
+++ src/main/java/dev/zanotelli/One/Shot/Creator/service/PersonagemService.java
@@ -1,14 +1,32 @@
 package dev.zanotelli.One.Shot.Creator.service;
 
+import dev.zanotelli.One.Shot.Creator.model.Personagem;
 import dev.zanotelli.One.Shot.Creator.repository.PersonagemRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PersonagemService {
 
     private final PersonagemRepository repository;
+    public List<Personagem> getAll;
 
     public PersonagemService(PersonagemRepository repository) {
         this.repository = repository;
+    }
+
+    public List<Personagem> getAll() {
+        return repository.findAll();
+    }
+
+
+    public Personagem save(Personagem personagem) {
+        return repository.save(personagem);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+
     }
 }
